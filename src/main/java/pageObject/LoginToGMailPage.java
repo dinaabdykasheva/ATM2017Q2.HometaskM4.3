6593c131d85dbf1a97ca8f71cf9856d8a85c3007
Dina_Abdykasheva <Dina_Abdykasheva@epam.com>
2017-08-10 17:44:04 +0600

    commit

--- src/main/java/pageObject/LoginToGMailPage.java
+++ src/main/java/pageObject/LoginToGMailPage.java
@@ -12,7 +12,6 @@ public class LoginToGMailPage extends AbstractPage{
     private static final By PASSWORD_INPUT_LOCATOR = By.name("password");
     private static final By NEXT_BUTTON_LOCATOR1 = By.id("passwordNext");
     private static final By LOGIN_PAGE_LOCATOR = By.xpath(".//*[@class= 'sfYUmb']");
-    public WebElementsUtils loginJSExecutor;
 
     public LoginToGMailPage(WebDriver driver) {
         super(driver);
@@ -23,7 +22,7 @@ public class LoginToGMailPage extends AbstractPage{
         driver.findElement(USERNAME_INPUT_LOCATOR).sendKeys(username);
         driver.findElement(NEXT_BUTTON_LOCATOR).click();
         driver.findElement(PASSWORD_INPUT_LOCATOR).sendKeys(password);
-        loginJSExecutor.executeJavaScript(driver, NEXT_BUTTON_LOCATOR1, "arguments[0].click()");
+        WebElementsUtils.executeJavaScript(driver, NEXT_BUTTON_LOCATOR1, "arguments[0].click()");
         return new AccountPage(driver);
     }
 
--- src/main/java/pageObject/WriteMailPage.java
+++ src/main/java/pageObject/WriteMailPage.java
@@ -16,26 +16,25 @@ public class WriteMailPage extends AbstractPage{
     private static final By DRAFTS_FOLDER_LOCATOR = By.xpath(".//a[contains(text(), 'Черновики')]");
     private static final By SAVING_LABEL_LOCATOR = By.xpath(".//span[@class = 'oG aOy']");
     private static final By TO_FIELD_IN_DRAFT_LOCATOR = By.xpath(".//span[@class='vN bfK a3q']");
-    public WebElementsUtils highlightExecutor, moveToReceiver, fillReceiver;
 
     public WriteMailPage(WebDriver driver) {
         super(driver);
     }
 
     public DraftsFolderPage writeMailAndSaveToDraft(String to, String subject, String body) {
-        fillReceiver.fillInField(driver, TO_FIELD_LOCATOR, to);
+        WebElementsUtils.fillInField(driver, TO_FIELD_LOCATOR, to);
         driver.findElement(SUBJECT_FIELD_LOCATOR).sendKeys(subject);
         driver.findElement(BODY_FIELD_LOCATOR).sendKeys(body);
         waitForElementPresent(ExpectedConditions.visibilityOfElementLocated(SAVING_LABEL_LOCATOR));
-        highlightExecutor.executeJavaScript(driver, SAVING_LABEL_LOCATOR, "arguments[0].style.border='3px solid green'");
-        highlightExecutor.executeJavaScript(driver, SAVING_LABEL_LOCATOR, "arguments[0].style.border='0px'");
+        WebElementsUtils.executeJavaScript(driver, SAVING_LABEL_LOCATOR, "arguments[0].style.border='3px solid green'");
+        WebElementsUtils.executeJavaScript(driver, SAVING_LABEL_LOCATOR, "arguments[0].style.border='0px'");
         driver.findElement(CLOSE_WRITE_MAIL_WINDOW_LOCATOR).click();
         driver.findElement(DRAFTS_FOLDER_LOCATOR).click();
         return new DraftsFolderPage(driver);
     }
 
     public String getReceiver() {
-        moveToReceiver.moveToField(driver, TO_FIELD_LOCATOR);
+        WebElementsUtils.moveToField(driver, TO_FIELD_LOCATOR);
         return driver.findElement(TO_FIELD_IN_DRAFT_LOCATOR).getAttribute("email");
     }
 
